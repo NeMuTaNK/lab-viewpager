88567c0ba3feaa4c6afaf7b4d3aecf22accce4cb
Nelson Tan <nelsontan@D-10-18-179-220.dhcp4.washington.edu>
2018-10-25 10:32:25 -0700

    recloned lab

--- app/src/main/java/edu/uw/viewpager/MainActivity.kt
+++ app/src/main/java/edu/uw/viewpager/MainActivity.kt
@@ -1,16 +1,33 @@
 package edu.uw.viewpager
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.app.FragmentTransaction
+import android.support.v4.view.PagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
+import android.support.v4.view.ViewPager
 
 class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListener {
+
+    private var searchFragment: SearchFragment? = null
+    private var listFragment: MovieListFragment? = null
+    private var detailFragment: DetailFragment? = null
+    private var viewPager: ViewPager? = null
+    private var pagerAdapter: PagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchFragment = SearchFragment.newInstance()
+
+        viewPager = findViewById<ViewPager>(R.id.pager)
+        pagerAdapter = MoviePagerAdapter(supportFragmentManager)
+        viewPager!!.adapter = pagerAdapter
     }
 
     //respond to search button clicking
@@ -40,5 +57,28 @@ class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListe
         private val TAG = "MainActivity"
         val MOVIE_LIST_FRAGMENT_TAG = "MoviesListFragment"
         val MOVIE_DETAIL_FRAGMENT_TAG = "DetailFragment"
+    }
+
+    private inner class MoviePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment? {
+            if (position == 0) return searchFragment
+            if (position == 1) return listFragment
+            return if (position == 2) detailFragment else null
+        }
+
+        override fun getItemPosition(`object`: Any?): Int {
+            return PagerAdapter.POSITION_NONE
+        }
+
+        override fun getCount(): Int {
+            return if (listFragment == null) {
+                1
+            } else if (detailFragment == null) {
+                2
+            } else {
+                3
+            }
+        }
     }
 }
